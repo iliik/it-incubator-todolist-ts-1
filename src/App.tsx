2e28afe16dc279fbbfe690cda6905ea294b2a2f1
Ilya <koselevicila@gmail.com>
2022-08-15 22:52:00 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./Todolist.JSX";
+
+function App() {
+    const title = "1111"
+    const title2 = "2222"
+
+    const tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ]
+    const tasks2 = [
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am Happy", isDone: false},
+        {id: 3, title: "Yo", isDone: false}
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title={title} titleNEW={100200} tasks={tasks1}/>
+            <Todolist title={title2} tasks={tasks2}/>
+        </div>
+    );
+}
+
+export default App;
