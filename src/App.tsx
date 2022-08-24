6d0118dce464d61e5f81660b6b9d018209f89147
Ilya <koselevicila@gmail.com>
2022-08-24 17:22:01 +0300

    comit-5

--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,26 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 
-
 export type FilterType = "All" | "Active" | "Completed"
 
 function App() {
-
-
-    let [tasks1, setTasks1] = useState([
+    let [tasks, setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
         {id: 4, title: "ReactJS2", isDone: false}
     ])
 
-    let [filterValue, setfilterderTasks] = useState("All")
-    let filterderTasks = tasks1
-    if (filterValue === 'Active') {
-        filterderTasks = tasks1.filter(el => !el.isDone)
-    }
-    if (filterValue === 'Completed') {
-        filterderTasks = tasks1.filter(el => el.isDone)
-    }
-
-
     const removeTask = (tasksid: number) => {
-        setTasks1(tasks1.filter((el) => el.id !== tasksid))
-    }
-
-
-    const FilderButton = (filterVal: string) => {
-        setfilterderTasks(filterVal)
-        console.log(filterVal)
+        setTasks(tasks.filter((el) => el.id !== tasksid))
     }
 
     return (
         <div className="App">
+
             <Todolist title="What to learn"
-                      tasks={filterderTasks}
-                      removeTask={removeTask}
-                      FilderButton={FilderButton}
-
-
-            />
+                      tasks={tasks}
+                      removeTask={removeTask}/>
         </div>
     );
 }
